82a3c47392420f8441b321eeba8196ad371a0b62
TomySpy <jtomas.perez@udea.edu.co>
2025-06-14 20:32:33 -0500

    Paciente_dicom

--- clase.py
+++ clase.py
@@ -10,3 +10,10 @@ class Paciente:
         self.edad = edad
         self.id_paciente = id_paciente
         self.imagen_3d = imagen_3d
+class ImagenDICOM:
+    def __init__(self, carpeta):
+        self.carpeta = carpeta
+        self.archivos = self._cargar_archivos()
+        self.dicom_objs = self._leer_dicom_objs()
+        self.volumen = self._reconstruir_volumen()
+
